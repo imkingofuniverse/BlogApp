444cedede33dc3db9f0d289a471e7a6aadcfcc42
Suyog Nimsatkar <suyog_pravin.nimsatkar@capgemini.com>
2023-06-19 11:42:23 +0530

    blog module

---
+++ src/main/java/com/blogapp/dto/CommentDto.java
@@ -0,0 +1,16 @@
+package com.blogapp.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    
+	private long id;
+
+    private String name;
+
+    private String email;
+
+    private String content;
+    
+}
