fa156f9319dc6c239777d7383b44ce39250a0341
harsh5181 <h.katkar2000@gmail.com>
2023-06-20 14:36:36 +0530

    Post + Comment

--- src/test/java/com/blogapp/servicetest/CommentServiceTestAddComment.java
+++ src/test/java/com/blogapp/servicetest/CommentServiceTestAddComment.java
@@ -1,81 +1,80 @@
 package com.blogapp.servicetest;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//
+//import java.util.Optional;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//import com.blogapp.dto.CommentDto;
+//import com.blogapp.entity.Comment;
+//import com.blogapp.entity.Post;
+//import com.blogapp.exception.PostNotFoundException;
+//import com.blogapp.repository.CommentRepository;
+//import com.blogapp.repository.PostRepository;
+//import com.blogapp.service.CommentService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import com.blogapp.dto.CommentDto;
-import com.blogapp.entity.Comment;
-import com.blogapp.entity.Post;
-import com.blogapp.exception.PostNotFoundException;
-import com.blogapp.repository.CommentRepository;
-import com.blogapp.repository.PostRepository;
-import com.blogapp.service.CommentService;
-
-@RunWith(MockitoJUnitRunner.class)
 public class CommentServiceTestAddComment {
-
-    @Mock
-    private CommentRepository commentRepository;
-
-    @Mock
-    private PostRepository postRepository;
-
-    @InjectMocks
-    private CommentService commentService;
-
-    @Before
-    public void setup() {
-        // Set up any necessary mock data or configurations
-    }
-
-    @Test
-    public void testCreateComment() throws PostNotFoundException {
-        // Arrange
-        long postId = 1L;
-
-        CommentDto mockCommentDto = new CommentDto();
-        // Set properties of mockCommentDto
-        mockCommentDto.setId(1L);
-        mockCommentDto.setName("Ram");
-        mockCommentDto.setEmail("ram@gmail.com");
-        mockCommentDto.setContent("nice post");
-
-        Comment mockComment = new Comment();
-        // Set properties of mockComment
-        mockComment.setId(1L);
-        mockComment.setName("Ram");
-        mockComment.setEmail("ram@gmail.com");
-        mockComment.setContent("nice post");
-
-        Post mockPost = new Post();
-        // Set properties of mockPost
-        mockPost.setId(1L);
-        mockPost.setTitle("MyBlog");
-        mockPost.setImage("URL");
-        mockPost.setContent("Text");
-        mockPost.setCategory("My Category");
-
-        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
-        when(commentRepository.save(any(Comment.class))).thenReturn(mockComment);
-
-        // Act
-        CommentDto result = commentService.createComment(postId, mockCommentDto);
-
-        // Assert
-        assertEquals(mockCommentDto, result);
+//
+//    @Mock
+//    private CommentRepository commentRepository;
+//
+//    @Mock
+//    private PostRepository postRepository;
+//
+//    @InjectMocks
+//    private CommentService commentService;
+//
+//    @BeforeEach
+//    public void setup() {
+//        // Set up any necessary mock data or configurations
+//    }
+//
+//    @Test
+//    public void testCreateComment() throws PostNotFoundException {
+//        // Arrange
+//        long postId = 1L;
+//
+//        CommentDto mockCommentDto = new CommentDto();
+//        // Set properties of mockCommentDto
+//        mockCommentDto.setId(1L);
+//        mockCommentDto.setName("Ram");
+//        mockCommentDto.setEmail("ram@gmail.com");
+//        mockCommentDto.setContent("nice post");
+//
+//        Comment mockComment = new Comment();
+//        // Set properties of mockComment
+//        mockComment.setId(1L);
+//        mockComment.setName("Ram");
+//        mockComment.setEmail("ram@gmail.com");
+//        mockComment.setContent("nice post");
+//
+//        Post mockPost = new Post();
+//        // Set properties of mockPost
+//        mockPost.setId(1L);
+//        mockPost.setTitle("MyBlog");
+//        mockPost.setImage("URL");
+//        mockPost.setContent("Text");
+//        mockPost.setCategory("My Category");
+//
+//        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+//        when(commentRepository.save(any(Comment.class))).thenReturn(mockComment);
+//
+//        // Act
+//        CommentDto result = commentService.createComment(postId, mockCommentDto);
+//
+//        // Assert
+//        assertEquals(mockCommentDto, result);
 //        assertEquals(mockPost, mockComment.getPost());
 //        verify(postRepository).findById(postId);
 //        verify(commentRepository).save(any(Comment.class));
-    }
+//    }
 }
